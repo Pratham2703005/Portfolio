41537ebd1f8c664844cc85d76f6410976a98f817
Pratham Israni <pk2732004@gmail.com>
2024-12-29 19:50:00 +0530

    routes problem

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router} from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/'>
+  <Router basename='/'>
     <Toaster/>
     <App />
-  </BrowserRouter>
+  </Router>
   
 )
