df19e62b8130cedd27079b7292e3229d3008448e
Pratham Israni <pk2732004@gmail.com>
2024-12-30 15:45:28 +0530

    homepage button fix

--- src/components/ui/GlitchButton.jsx
+++ src/components/ui/GlitchButton.jsx
@@ -2,7 +2,7 @@
 import './GlitchButton.css';
 const GlitchButton = ({children, className, onClick}) => {
   return (
-    <button className={`!px-2 sm:px-[50px] ${className}`} onClick={onClick} id="glitch"  >
+    <button className={`!px-4 sm:!px-12 ${className}`} onClick={onClick} id="glitch"  >
     {children}
     </button>
   )
